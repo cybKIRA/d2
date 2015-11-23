f8937815bb78aad59ebb3137a73472d05d7b175f
xandeadx <xxandeadxx@gmail.com>
2015-11-23 23:54:35 +0300

    Fix js

--- commerce_acf.js
+++ commerce_acf.js
@@ -2,16 +2,22 @@
   Drupal.behaviors.commerceAcf = {
     attach: function (context, settings) {
       if ($('.view-commerce-cart-form').length > 0) {
-        $('.view-commerce-cart-form .quantity-with-spinner')
-          .spinner({
-            min: 1,
-            stop: function(event, ui) {
-              $(event.target).keyup();
-            }
-          })
-          .keyup(function() {
+        $('.view-commerce-cart-form input[name^="edit_quantity"]').each(function() {
+          var $element = $(this);
+
+          $element.keyup(function() {
             Drupal.commerceAcf.quantityChanged(this);
           });
+
+          if ($element.hasClass('quantity-with-spinner')) {
+            $element.spinner({
+              min: 1,
+              stop: function(event, ui) {
+                $(event.target).keyup();
+              }
+            })
+          }
+        });
       }
     }
   };
